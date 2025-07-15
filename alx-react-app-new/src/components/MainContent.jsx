374ffa640c961e4fc417cb9bbccd3838ecde9eac
Fiona Wambua <fionawambua20@gmail.com>
2025-07-15 12:38:27 +0300

    new changes

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <div
+      style={{
+        padding: '20px', backgroundColor: '#e0f7fa', color: '#333', fontSize: '18px', borderRadius: '10px', margin: '20px', textAlign: 'center',boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      }}
+    >
+      <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </div>
+  );
+}
+
+export default MainContent;
