7eb4a04ff1b55311dd45cccdd32bf9c3490de412
Fiona Wambua <fionawambua20@gmail.com>
2025-07-15 14:00:36 +0300

    new files

---
+++ my-company/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Services from './Services'
+import Contact from './Contact'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: "20px", minHeight: "80vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  const style = {
+    textAlign: "center",
+    padding: "10px",
+    backgroundColor: "#f0f0f0",
+    marginTop: "20px",
+  };
+
+  return (
+    <footer style={style}>
+      <p>© 2025 MyCompany. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
