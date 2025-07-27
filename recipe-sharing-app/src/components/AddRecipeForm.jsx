5a8037058f244fd90ac02337d4b40934f2776286
Fiona Wambua <fionawambua20@gmail.com>
2025-07-27 14:21:23 +0300

    new file

---
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useRecipeStore } from "../store/RecipeStore";
+
+const AddRecipeForm = () => {
+  const addRecipe = useRecipeStore((state) => state.addRecipe);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !description) return;
+
+    addRecipe({ id: Date.now(), title, description });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        required
+      />
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+};
+
+export default AddRecipeForm;
