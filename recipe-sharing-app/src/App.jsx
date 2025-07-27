165e1cafce99bfa0873547bc3e3717f0b540c66a
Fiona Wambua <fionawambua20@gmail.com>
2025-07-27 14:48:52 +0300

    new changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,18 @@
-import AddRecipeForm from "./components/AddRecipeForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div style={{ padding: "2rem", maxWidth: "600px", margin: "auto" }}>
-      <h1>🍳 My Recipe App</h1>
+    <Router>
+      <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
-      <RecipeList />
-    </div>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
