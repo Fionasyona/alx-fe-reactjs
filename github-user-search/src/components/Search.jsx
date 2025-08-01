08d264e2cca581890ddc92ed9f6db2255f049cdc
Fiona Wambua <fionawambua20@gmail.com>
2025-08-01 15:17:51 +0300

    updates

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUser(data);
     } catch (error) {
-      setError("Looks like we can’t find the user.");
+      setError("Looks like we cant find the user.");
     } finally {
       setLoading(false);
     }
