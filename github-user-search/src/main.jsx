433f2843d202557eac2bcf6805adcfb2399f6b29
Fiona Wambua <fionawambua20@gmail.com>
2025-08-01 14:36:48 +0300

    new adds

---
+++ github-user-search/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
