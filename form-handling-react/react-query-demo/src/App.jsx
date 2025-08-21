a3965631c4933f7ef982d2edcd435a3026d4db65
Fiona Wambua <fionawambua20@gmail.com>
2025-08-21 11:06:47 +0300

    new changes

--- form-handling-react/react-query-demo/src/App.jsx
+++ form-handling-react/react-query-demo/src/App.jsx
@@ -1,8 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import PostsComponent from "./components/PostsComponent";
+import PostsComponent from './component/PostsComponent.jsx';  // Adjust the import path as necessary
+import './index.css';  // Ensure you have your styles imported  
+import ReactDOM from 'react-dom/client';
+
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <PostsComponent />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
