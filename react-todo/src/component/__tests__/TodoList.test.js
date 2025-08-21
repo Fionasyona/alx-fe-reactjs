0877c4d2df16a2afbf77a857d07edb3f11f83575
Fiona Wambua <fionawambua20@gmail.com>
2025-08-21 16:43:17 +0300

    changes

--- react-todo/src/component/__tests__/TodoList.test.js
+++ react-todo/src/component/__tests__/TodoList.test.js
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import TodoList from "../components/TodoList";
+import TodoList from "../../components/TodoList";
 
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
